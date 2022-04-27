ac2eacafdeff1f6a1966d76f3bacb55a0da156e0
Ion G <98644414+Johny36@users.noreply.github.com>
2022-04-27 11:21:34 +0300

    Add files via upload

---
+++ Cycle-While-Int-Number.py
@@ -0,0 +1,17 @@
+
+start_n = int(input("Enter start number: "))
+end_n = int(input("Enter end number: "))
+
+
+print("\n")
+
+while start_n < end_n:
+    print(start_n, end = " ")
+    start_n += 1
+while start_n >= end_n:
+    print(start_n, end = " ")
+    start_n -= 1         
+    
+
+
+print("\n")
